fa9347eef2f2addb7ad84cab9fec2c9dcc62db1c
itsikbenishu <itsikbe1@gmail.com>
2025-05-23 18:00:02 +0300

    fix bug at chattels class

--- entities/Chattels/Chattels.py
+++ entities/Chattels/Chattels.py
@@ -6,18 +6,3 @@ class Chattels(HomeDoc):
     colors: str
     quantity: str
     weight: str
-
-    def get_by_id(self, item_id):
-        return self.repo.get_by_id(item_id)
-
-    def get(self):
-        return self.repo.get()
-
-    def create(self, data):
-        return self.repo.create(data)
-
-    def update(self, item_id, data):
-        return self.repo.update(item_id, data)
-
-    def delete(self, item_id):
-        return self.repo.delete(item_id)
